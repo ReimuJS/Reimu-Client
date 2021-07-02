5005c75c342d5c9a31675aa99bb34ddfdf84249b
Super Fire <starjumper.pe@gmail.com>
2021-07-02 17:59:12 +0700

    feat(client): ping feature and Reconnection

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,23 @@ import Message from "./Message/index";
 import * as msgpack from "msgpack-lite";
 
 export class Reimu extends EventEmitter {
-  constructor(url: string) {
+  constructor(
+    url: string,
+    options?: {
+      pingTimeout?: number;
+      responseTimeout?: number;
+      reconnects?: number;
+      reconnectTimeout?: number;
+    }
+  ) {
     super();
+
+    this.options = {
+      pingTimeout: options.pingTimeout || 5000,
+      responseTimeout: options.responseTimeout || 15000,
+      reconnects: options.reconnects || 10,
+      reconnectTimeout: options.reconnectTimeout || 40000,
+    };
 
     this.url = url;
     this.connect();
@@ -15,14 +30,34 @@ export class Reimu extends EventEmitter {
   // Variables
 
   private url;
+  private pingTimer?: NodeJS.Timeout;
+  private reconnectTimer?: NodeJS.Timeout;
   private ws?: WebSocket;
   private messageId;
   private connected: boolean;
+  private reconnects: { reconnects: number; lastReconnect: Date } = {
+    reconnects: 0,
+    lastReconnect: new Date(),
+  };
+
+  /**
+   * Options to be used
+   * @type {Object<string, number>}
+   */
+  public options: {
+    pingTimeout: number;
+    responseTimeout: number;
+    reconnects: number;
+    reconnectTimeout: number;
+  };
 
   /**
    * Last sent packet
    */
-  public lastContact: { client: Date; server: Date };
+  public lastContact: { client: Date; server: Date } = {
+    client: new Date(),
+    server: new Date(),
+  };
 
   /**
    * Close info
@@ -98,7 +133,6 @@ export class Reimu extends EventEmitter {
 
     this.ws.addEventListener("open", this.open);
     this.ws.addEventListener("message", this.message);
-    this.ws.addEventListener("error", this.error);
     this.ws.addEventListener("close", this.onClose);
   };
 
@@ -130,8 +164,25 @@ export class Reimu extends EventEmitter {
     );
   };
 
-  private open = (event: Event) => {};
+  private open = (event: Event) => {
+    this.connected = true;
+    this.close = undefined;
+
+    this.pingTimer = setInterval(() => {
+      this.ping();
+    }, 500);
+
+    this.reconnectTimer && clearInterval(this.reconnectTimer);
+
+    this.lastContact = { client: new Date(), server: new Date() };
+
+    this.sendRaw({ type: "batch", data: this.droppedPackets });
+    this.emit("open");
+  };
+
   private message = (event: MessageEvent) => {
+    this.lastContact.server = new Date();
+
     let decoded: any;
     try {
       decoded = msgpack.decode(new Uint8Array(event.data));
@@ -143,15 +194,72 @@ export class Reimu extends EventEmitter {
 
     new Message(decoded, this);
   };
-  private error = (event: Event) => {};
+
   private onClose = (event: CloseEvent) => {
+    !this.close && (this.close = { code: event.code, server: "unexpected" });
+
+    this.Reconnect();
+  };
+
+  private Reconnect = () => {
+    if (!this.close) return;
+
+    this.pingTimer && clearInterval(this.pingTimer);
+
+    if (
+      new Date().getTime() - this.reconnects.lastReconnect.getTime() >
+      this.options.reconnectTimeout
+    )
+      this.reconnects.reconnects = 0;
+    this.reconnects.reconnects++;
+
+    this.reconnects.lastReconnect = new Date();
+
+    if (this.reconnects.reconnects > this.options.reconnects) {
+      this.emit("close", this.close?.code, !!this.close?.server);
+      return;
+    }
+
     this.ws.removeEventListener("open", this.open);
     this.ws.removeEventListener("message", this.message);
-    this.ws.removeEventListener("error", this.error);
     this.ws.removeEventListener("close", this.onClose);
 
-    this.emit("close", this.close.code || event.code, !!this.close.server);
     this.connected = false;
+
+    if (["unexpected", "ping"].includes(this.close.server.toString())) {
+      this.emit("reconnecting");
+      this.connect();
+
+      this.reconnectTimer = setTimeout(() => {
+        this.emit("close", this.close?.code, !!this.close?.server);
+      }, this.options.reconnectTimeout);
+    } else {
+      this.emit("close", this.close.code, this.close.server);
+    }
+  };
+
+  private ping = () => {
+    let pingTimeout = this.options.pingTimeout;
+
+    if (this.close) {
+      return;
+    }
+
+    // Safely assume a ping would be sent
+    if (
+      new Date().getTime() - this.lastContact.client.getTime() >
+      pingTimeout / 2
+    ) {
+      this.sendRaw({ id: this.messageId++, type: "ping" });
+    } else {
+      // Now, check time between last client send and last server send
+      if (
+        this.lastContact.client.getTime() - this.lastContact.server.getTime() >
+        pingTimeout
+      ) {
+        this.Reconnect();
+      }
+    }
   };
 
   /**
@@ -172,6 +280,31 @@ export class Reimu extends EventEmitter {
   public acknoledge(message: Message): void {
     this.sendRaw({ for: message.id, type: "acknoledge" });
   }
+}
+
+export default interface WebSocketManager {
+  /**
+   * Emitted when the connection recieves a message
+   */
+  on(event: "message", callback: (message: Message) => void): this;
+
+  /**
+   * Emitted when the connection is closed
+   */
+  on(
+    event: "close",
+    callback: (code: number, server: boolean | "unexpected" | "ping") => void
+  ): this;
+
+  /**
+   * Emitted when the connection opened or reconnected
+   */
+  on(event: "open", callback: () => void): this;
+
+  /**
+   * Emitted when the connection is reconnecting
+   */
+  on(event: "reconnecting", callback: () => void): this;
 }
 
 interface DecodedMessage {
