928520d6fb71f0cb37ed9415e6b55f0a4dae788a
Super Fire <36259493+Sup3rFire@users.noreply.github.com>
2021-11-26 18:12:38 +0700

    Add the start of the code

--- src/client.ts
+++ src/client.ts
@@ -1,1 +1,42 @@
-export default function Client() {}
+export default function Client<MessageType>(
+  endpoint: string,
+  options?: Partial<options<MessageType>>
+) {
+  const opts: options<MessageType> = {
+    reconnectTimeout: 40,
+    ...options,
+  };
+
+  let ws: WebSocket;
+  let id: string | null = null;
+
+  function Open(newId: boolean = false) {
+    if (newId) id = null;
+    if (ws) {
+      // Discard the old socket entirely
+      ws.onopen = () => {};
+      ws.onmessage = () => {};
+      ws.onerror = () => {};
+      ws.onclose = () => {};
+      ws.close();
+    }
+
+    ws = new WebSocket(endpoint);
+    ws.binaryType = "arraybuffer";
+
+    ws.onopen = () => {
+      if (newId || !id) {
+        ws.send("hello");
+      } else {
+        ws.send(id);
+      }
+    };
+  }
+
+  Open(true);
+}
+
+export interface options<MessageType> {
+  /** Maximum time in seconds that the client can be disconnected before it will no longer be allowed to reconnect. Defaults to 40. */
+  reconnectTimeout: number;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
 import Client from "./client";
 
 export { Client };
+
+export enum rawTypes {
+  ACK,
+  UDATA,
+  URES,
+  UBUF,
+}
+
+export function numToHex(num: number): Buffer {
+  let hex = num.toString(16);
+  if (hex.length % 2) {
+    hex = "0" + hex;
+  }
+  const numHex = Buffer.from(hex, "hex");
+  return Buffer.concat([Buffer.from([numHex.length - 1]), numHex]);
+}
